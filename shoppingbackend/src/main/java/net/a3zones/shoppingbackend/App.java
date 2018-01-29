1969de1b2387de1b118b54085473889535ad303e
sulav agarwal <sulav.147@gmail.com>
2018-01-29 14:21:05 +0530

    My project setup completed successfully

---
+++ shoppingbackend/src/main/java/net/a3zones/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package net.a3zones.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
